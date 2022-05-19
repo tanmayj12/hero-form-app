06e7c9810076338afa521ae12db26e393ba31a77
Tanmay <jtanmay12@gmail.com>
2022-05-20 02:05:18 +0530

    first commit hero form

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent implements OnInit {
+
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreat');
+  submitted = false;
+  onSubmit() { this.submitted = true };
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
